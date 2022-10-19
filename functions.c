2682022d8da3b033adeb532a0e3fa894c2e1cdd8
tracilvrd <oghenekparobo.ese@gmail.com>
2022-10-18 17:50:17 -0700

    second trial

--- functions.c
+++ functions.c
@@ -138,8 +138,7 @@ int print_int(va_list types, char buffer[],
 
 	i++;
 
-	return (write_number(is_negative, i, buffer, flags, width, precision,
-		size));
+	return (write_number(is_negative, i, buffer, flags, width, precision, size));
 }
 
 /************************* PRINT BINARY *************************/
